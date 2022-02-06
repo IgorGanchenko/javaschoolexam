e02aadde7361a5b27147c937cdb542d9f42252fb
IgorGanchenko <navel86@mail.ru>
2022-02-06 10:18:22 +0300

    PyramidBuilder ready

--- src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
+++ src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
@@ -1,5 +1,7 @@
 package com.tsystems.javaschool.tasks.calculator;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Calculator {
 
     /**
@@ -10,9 +12,81 @@ public class Calculator {
      *                  Example: <code>(1 + 38) * 4.5 - 1 / 2.</code>
      * @return string value containing result of evaluation or null if statement is invalid
      */
+
     public String evaluate(String statement) {
-        // TODO: Implement the logic here
+        //parseString(statement);
+        String sub = findParentheses(statement);
+        System.out.println(sub);
+//        String[] parts = statement.split("\\s");
+//        for (int i = 0; i < parts.length; i++) {
+//            System.out.println(parts[i]);
+//        }
         return "";
     }
 
+    private String findParentheses(String statement) {
+        int index = statement.indexOf('(');
+        int index2 = statement.indexOf(')');
+        String sub = "";
+        if (index != -1) {
+            sub = statement.substring(index + 1, index2);
+        }
+        return sub;
+    }
+
+    private String findSign(String statement, char calculatingChar) {
+        int indexMinus = statement.indexOf(calculatingChar);
+        if (indexMinus != -1 && indexMinus != 0) {
+            String[] parts = statement.split(String.valueOf("\\" + calculatingChar));
+            String resultValue = parts[0];
+            if (parts.length > 1) {
+                for (int i = 1; i < parts.length; i++) {
+                    resultValue = calculateStrings(resultValue, parts[i], calculatingChar);
+                }
+            }
+            return String.valueOf(resultValue);
+        }
+        return statement;
+    }
+
+    private String calculateStrings(String str1, String str2, char calculatingChar) {
+        int value1 = Integer.parseInt(str1);
+        int value2 = Integer.parseInt(str2);
+        int result = 0;
+        switch (calculatingChar) {
+            case ('+'):
+                result = value1 + value2;
+                break;
+            case ('-'):
+                result = value1 - value2;
+                break;
+            case ('*'):
+                result = value1 * value2;
+                break;
+            case ('/'):
+                if (value2 >= 0) {
+                    result = value1 / value2;
+                } else {
+                    return null;
+                }
+                break;
+        }
+        return String.valueOf(result);
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        String statement = "(1 + 38) * 4.5 - 1 / 2";
+        statement = statement.replaceAll("\\s+","");
+
+        String newString = calculator.findParentheses(statement);
+        statement = statement.replace(newString, calculator.findSign(newString, '+'));
+        System.out.println(statement);
+
+
+
+
+        //calculator.evaluate("(1 + 38) * 4.5 - 1 / 2");
+    }
 }
--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -1,5 +1,7 @@
 package com.tsystems.javaschool.tasks.pyramid;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class PyramidBuilder {
@@ -13,9 +15,54 @@ public class PyramidBuilder {
      * @throws {@link CannotBuildPyramidException} if the pyramid cannot be build with given input
      */
     public int[][] buildPyramid(List<Integer> inputNumbers) {
-        // TODO : Implement your solution here
-        return new int[0][0];
+        pyramidChecking(inputNumbers);
+        Collections.sort(inputNumbers);
+
+        int rows = getNumbersOfRows(inputNumbers);
+        int columns = rows * 2 - 1;
+        int[][] pyramid = new int[rows][columns];
+
+        int counter = 0;
+        for (int i = 0; i < rows; i++) {
+            int firstIndex = columns / 2 - i;
+            int necessaryQty = i + 1;
+            for (int j = 0; j < columns; j++) {
+                if (necessaryQty > 0) {
+                    if (counter < inputNumbers.size()) {
+                        if (firstIndex == j) {
+                            pyramid[i][j] = inputNumbers.get(counter);
+                            counter++;
+                            firstIndex += 2;
+                            necessaryQty--;
+                        }
+                    }
+                } else {
+                    pyramid[i][j] = 0;
+                }
+            }
+        }
+        return pyramid;
     }
 
+    private void pyramidChecking(List<Integer> inputNumbers) {
+        if (inputNumbers.contains(null) || !checkIfPossibleCreate(inputNumbers)) {
+            throw new CannotBuildPyramidException();
+        }
+    }
+
+    private boolean checkIfPossibleCreate(List<Integer> inputNumbers) {
+        int row = getNumbersOfRows(inputNumbers);
+        int qty = inputNumbers.size();
+        double necessaryQty = (1 + row) / 2.0 * row;
+        return (necessaryQty == qty);
+    }
+
+    private int getNumbersOfRows(List<Integer> inputNumbers) {
+        int rows = 0;
+        for (int i = 0; i < inputNumbers.size(); i = rows + i) {
+            rows++;
+        }
+        return rows;
+    }
 
 }
