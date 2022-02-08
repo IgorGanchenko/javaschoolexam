111c8ef83efb37623a490f30779fe6a926552000
IgorGanchenko <navel86@mail.ru>
2022-02-08 17:59:53 +0300

    Subsequence ready

--- src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
+++ src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
@@ -1,6 +1,9 @@
 package com.tsystems.javaschool.tasks.subsequence;
 
 import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class Subsequence {
 
@@ -13,8 +16,25 @@ public class Subsequence {
      * @return <code>true</code> if possible, otherwise <code>false</code>
      */
     @SuppressWarnings("rawtypes")
-    public boolean find(List x, List y) {
-        // TODO: Implement the logic here
-        return false;
+    public boolean find(List x, List y) throws IllegalArgumentException {
+        checkTheListsToNull(x);
+        checkTheListsToNull(y);
+        for (int i = 0; i < x.size(); i++) {
+            int index = y.indexOf(x.get(i));
+            if (index != -1) {
+                y = y.subList(index, y.size());
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
+
+    private void checkTheListsToNull(List y) {
+        if (y == null){
+            throw new IllegalArgumentException();
+        }
+    }
+
+
 }
